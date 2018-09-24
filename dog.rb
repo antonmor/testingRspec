159b025d1ddbaf341c78f7d146b1bbf698894948
antonmor <ingantonmor@gmail.com>
2018-09-24 11:08:09 -0500

    Finalizando test

--- dog.rb
+++ dog.rb
@@ -1,2 +1,14 @@
 class Dog
+  def initialize(hunger_level: 0)
+    @hunger_level = hunger_level
+  end
+
+  def bark
+   return "Woof!" 
+  end
+
+  def hungry?
+    @hunger_level > 5
+  end
+
 end
--- dog_spec.rb
+++ dog_spec.rb
@@ -1,5 +1,26 @@
 require 'rspec'
 require_relative 'dog'
 
-describe Dog do
+describe Dog do 
+  subject { Dog.new }
+
+  describe '#bark' do
+    it 'returns the "Woof!"' do
+    #  dog = Dog.new
+      expect(subject.bark).to eql('Woof!')
+    end
+  end
+
+  describe '#hungry?' do
+    it 'returns true if hunger_level is more than 5' do
+      dog = Dog.new(hunger_level: 7)
+      expect(dog).to be_hungry
+    end 
+
+    it 'returns falase if hunger_level is 5 or less' do
+      dog = Dog.new(hunger_level: 5)
+      expect(dog).to_not be_hungry
+    end 
+
+  end
 end
